9c388dfa77804d7560dfb98dd6508279da72fb95
Davor Vujanovic <vujanovicdavor8@gmail.com>
2020-05-25 14:21:39 +0200

    a

--- java.js
+++ java.js
@@ -6,15 +6,58 @@ function CheckUser(name, password){
 
 function Login(username, password){
     var data = document.getJSON("http://localhost:3000/users");
-
-    console.log(data[1]);
-    if(username === saveedUsername.name && data[0] === password)
-    {
-        console.log('yeah');
-        
-    }
+    var username = document.getElementById('username').valeue;
+    var password = document.getElementById('password').value;
 }
 
 function Write(){
-    console.log('yeah');
+    
+}
+
+function card(){
+
+}
+
+function loadProduct(){
+
+    var request = newXHTMLRequest();
+    const products = document.getElementById('Nike');
+    var container = document.getElementById('div');
+    products.appendChild(container);
+
+    request.open('GET', 'http://localhost:3000/products')
+    request.onload = funtion()
+    {
+        var card;
+        var data = JSON.parse(this.response);
+
+        for(var i = 0; i < data.length; i++)
+        {
+            card = document.createElement('div');
+            card.setAttribute('class', 'card');
+
+            var prodimg = document.createElement('img');
+            prodimg.src = data[i].imagename;
+
+            var prdctname = document.createElement('h1');
+            prdctname.textContent = data[i].productname;
+
+            var prize = document.createElement('p');
+            prize.setAttribute('class', 'prize');
+            prize.textContent = data[i].productprize;
+
+            var button = document.createElement('button');
+            button.textContent = "Add To Cart";
+
+            container.appendChild(card);
+            card.appendChild(img);
+            card.appendChild(prdctname);
+            card.appendChild(prize);
+            card.appendChild(button);
+
+            var abstand = document.createElement('br');
+            container.appendChild(abstand);
+        }
+    }
+request.send();
 }
---
+++ test.js
@@ -0,0 +1,84 @@
+function add() {
+
+    var request = new XMLHttpRequest();
+    var products = document.getElementById('prods').getElementsByTagName('tbody')[0];
+    var data;
+    request.open('GET', 'http://localhost:3000/products', true);
+    request.onload = function () {
+        if (request.status < 200 || request.status > 400) { console.log("error: connection failed"); return; }
+        var card;
+        var trow;
+        data = JSON.parse(this.response);
+        var counter = 0;
+
+        for (var i = 0; i < data.length; i++) {
+            counter++;
+
+            if (counter === 1) {
+                trow = products.insertRow();
+            }
+
+            var tcell = trow.insertCell(counter - 1);
+
+            card = document.createElement('div');
+            card.setAttribute('class', 'card h-100');
+            card.style = "width:330";
+
+            var prodimg = document.createElement('img');
+            prodimg.src = data[i].img;
+            prodimg.setAttribute('class', 'card-img-top');
+
+            var cardBody = document.createElement('div');
+            cardBody.setAttribute('class', 'card-body');
+
+            var prdctname = document.createElement('h5');
+            prdctname.setAttribute('class', 'card-title');
+            prdctname.textContent = data[i].productName;
+            prdctname.id = data[i].id;
+
+            var prize = document.createElement('p');
+            prize.setAttribute('class', 'card-text');
+            prize.textContent = data[i].price + ",-";
+
+            var button = document.createElement('button');
+            button.setAttribute('class', 'btn btn-primary');
+            button.textContent = "Add To Cart";
+            button.onclick = searchInShopCart(data, i);
+            button.id = data[i].id;
+
+            tcell.appendChild(card);
+            card.appendChild(prodimg);
+            card.appendChild(cardBody);
+            cardBody.appendChild(prdctname);
+            cardBody.appendChild(prize);
+            cardBody.appendChild(button);
+
+            if (counter === 3) {
+                counter = 0;
+            }
+        }
+    }
+    request.send();
+}
+
+
+
+
+
+function searchInShopCart(data, id) {
+
+    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+    var theUrl = "http://localhost:3000/cart";
+
+    xmlhttp.open("POST", theUrl);
+
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send(JSON.stringify({
+
+        "productName": data[id].productName,
+        "price": data[id].price,
+    }));
+
+}
+
+add();
